cbf204782c254ad937427fa705af9e104b33767f
Dima Frolenko <mail@frolenkodima.ru>
2023-08-24 21:28:37 +0300

    feat: now one function parses body, and post functions

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func init() {
 	//and wont create router again
 	router = gin.Default()
 	router.GET("/", MyHandler)
-	router.GET("/stuff", MyHandler2)
+	router.POST("/stuff", PostHandler)
 }
 
 func main() {
@@ -27,15 +27,23 @@ func main() {
 }
 
 func MyHandler(c *gin.Context) {
-	//user := c.Params.ByName("name")
-
-	c.JSON(http.StatusOK, gin.H{"user": "val", "value": "test"})
+	c.JSON(http.StatusOK, gin.H{"user": "", "value": "test"})
 }
 
-func MyHandler2(c *gin.Context) {
-	//user := c.Params.ByName("name")
+// this function handles post requests to http://your.site/stuff
+// it reads body as json,
+// and returns its field "name"
+func PostHandler(c *gin.Context) {
+	type Request struct {
+		Name string `json:"name"`
+	}
+	var r Request
+	if err := c.ShouldBindJSON(&r); err != nil {
+		fmt.Println("Received bad data")
+		c.String(http.StatusBadRequest, err.Error())
+	}
 
-	c.JSON(http.StatusOK, gin.H{"user": "stuff", "value": "its working"})
+	c.JSON(http.StatusOK, gin.H{"user": r.Name, "value": "its working"})
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
